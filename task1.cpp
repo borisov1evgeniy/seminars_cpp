9852e61642e2a4349e7b3e03d58ee5afefc7d995
aaa <g55rus@mail.ru>
2024-10-06 13:31:12 +0300

    task1

---
+++ task1.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <string>
+void triangle(long long n) {
+	for (int i = 1; i < n*2; i++) {
+		if (i <= n) {
+			std::cout << std::string (i, '*') << std::endl;
+		}
+		else {
+			
+			std::cout << std::string (n*2-i,'*') << std::endl;
+		}
+	}
+	
+}
+int main() {
+	long long n;
+	std::cin >> n;
+	triangle(n);
+	return 0;
+}
